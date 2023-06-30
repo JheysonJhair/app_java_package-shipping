575c8acf07d0d7ce543bb8631457feff91fc23f8
JheysonJhair <jhair2003unamba@gmail.com>
2023-06-30 11:44:41 -0500

    Agregando el main y las imagenes de diseño

---
+++ src/SistemaEncomienda/SistemaEncomienda.java
@@ -0,0 +1,17 @@
+package SistemaEncomienda;
+
+import Vista.Login;
+
+/**
+ *
+ * @author Jhair
+ */
+public class SistemaEncomienda {
+
+    public static void main(String[] args) {
+        //Acceso al sistema
+        Login lg = new Login();
+        lg.setVisible(true);
+        
+    } 
+}
